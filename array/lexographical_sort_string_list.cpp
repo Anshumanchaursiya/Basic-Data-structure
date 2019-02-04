e5b04fa1ffcde4c4314229cb3355d687cc7c4411
Anshuman chaursiya <ansh9010@gmail.com>
2019-02-04 02:48:06 -1000

    Update lexographical_sort_string_list.cpp

--- array/lexographical_sort_string_list.cpp
+++ array/lexographical_sort_string_list.cpp
@@ -3,6 +3,11 @@
 #include<cstring>
 using namespace std;
 
+bool compare(string s1,string s2){
+	return s1>s2;
+}
+
+
 int main(){
 
 	string s[10] = {
@@ -16,7 +21,15 @@ int main(){
 	};
 
 	sort(s,s+7);
-	cout<<"Sorted strings: "<<endl<<endl;
+	cout<<" In assending order Sorted strings: "<<endl<<endl;
+	for(int i=0;i<7;i++){
+		cout<<s[i]<<endl;
+	}
+	cout<<endl;
+	
+	sort(s,s+7,compare);
+	cout<<"In discending order: "<<endl;
+
 	for(int i=0;i<7;i++){
 		cout<<s[i]<<endl;
 	}
