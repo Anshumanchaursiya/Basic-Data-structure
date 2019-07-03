8acbf0828bd320e82befa5963114e5e037a564ba
Anshuman <ansh9010@gmail.com>
2019-07-03 01:08:59 -1000

    added inorder postorder printing of tree

--- tree/tree_imp.cpp
+++ tree/tree_imp.cpp
@@ -49,11 +49,50 @@ void print(node* root){
 	return;
 }
 
+//Inorder printing of tree
+void printIn(node* root){
+
+	if(root == NULL){
+		return;
+	}
+
+	//otherwise print left root right
+	printIn(root->left);
+	cout<<root->data<<" ";
+	printIn(root->right);
+
+	return;
+
+}
+
+//Postorder printing of tree
+void printPost(node* root){
+
+	if(root == NULL){
+		return;
+	}
+
+	//otherwise print left right root
+	printPost(root->left);
+	
+	printPost(root->right);
+	cout<<root->data<<" ";
+
+	return;
+
+}
+
+
 int main(){
 
 	node* root = BuildTree();
-
+	cout<<"Preorder-> ";
 	print(root);
+	cout<<endl<<"Inorder-> ";
+	printIn(root);
+	cout<<endl<<"Postorder-> ";
+	printPost(root);
+	cout<<endl;
 
 	return 0;
 }
